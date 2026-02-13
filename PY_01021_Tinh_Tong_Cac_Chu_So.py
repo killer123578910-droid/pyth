e6e040626f0004a4014af2bd9a7796b0588f0abb
toibingu <killer123578910@gmail.com>
2026-02-13 23:11:53 +0700

    13/2/2026

--- PY_01021_Tinh_Tong_Cac_Chu_So.py
+++ PY_01021_Tinh_Tong_Cac_Chu_So.py
@@ -1,3 +1,17 @@
+import heapq
 def sovle():
     n=input()
-    
+    ans=[]
+    summ=0
+    i=0
+    while i<len(n):
+        if n[i].isdigit():
+            summ+=int(n[i])
+        else:
+            heapq.heappush(ans,n[i])
+        i+=1
+    while ans:
+        print(heapq.heappop(ans),end='')
+    print(summ)
+for k in range(int(input())):
+    sovle()
