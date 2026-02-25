c30d8fee60d73151a6a2de9ea697762609d90437
Khuong <killer123578910@gmail.com>
2026-02-25 17:45:06 +0700

    25/2/2026

---
+++ PY_02039_Ma_Tran_1.py
@@ -0,0 +1,16 @@
+n=int(input())
+ar=[]
+for i in range(n):
+    ar.append(input().split())
+k=int(input())
+add1=0
+add2=0
+for i in range(n):
+    for j in range(n):
+        if i<j:
+            add1+=int(ar[i][j])
+        elif i>j:
+            add2+=int(ar[i][j])
+ans=abs(add1-add2)
+print("YES" if ans<=k else "NO")
+print(ans)
