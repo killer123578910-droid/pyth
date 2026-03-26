113d95747988fe51a258504410d7683141e49aa7
Khuong <killer123578910@gmail.com>
2026-03-26 10:11:38 +0700

    26/3/2026

--- PYKT_091_Tim_Tu_Thuan_Nghich_Dai_Nhat.py
+++ PYKT_091_Tim_Tu_Thuan_Nghich_Dai_Nhat.py
@@ -1,16 +1,24 @@
 def thuannghich(n):
-    return n==n[:-1]
-f=open("VANBAN.in",'r')
-dic={}
-maxlen=0
-chose=''
+    return n == n[::-1]
+
+f = open("VANBAN.in",'r')
+
+dic = {}
+maxlen = 0
+chose = ''
+
 for i in f:
-    k=i.split()
-    for e in k:
-        if(thuannghich(e) and maxlen<len(e)):
-            if e not in dic:
-                dic[e]=0
-            dic[e]+=1
-            chose=e
-            maxlen=len(e)
-print(chose+" "+str(dic[chose]))
+    for e in i.split():
+        if thuannghich(e):   # ❗ bỏ điều kiện maxlen
+            if len(e) > maxlen:
+                dic.clear()
+                dic[e] = 1
+                chose = e
+                maxlen = len(e)
+            elif len(e) == maxlen:
+                if e not in dic:
+                    dic[e] = 0
+                dic[e] += 1
+
+for i in dic:
+    print(i,dic[i])
